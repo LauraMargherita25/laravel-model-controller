73856ff65e7c9708db9c3515947bf20464e55019
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-05-04 17:26:40 +0200

    Got movies from database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        $myMovies = Movie::all();
+        dd($myMovies);
+        return view('HomePage');
+    }
 }
